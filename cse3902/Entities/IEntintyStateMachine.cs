0a3dae46d80f7c80752524a3f29a9c66c722b60a
Dev Patel <dvptl68@gmail.com>
2021-02-03 20:18:09 -0500

    Add entinty state machine interface

---
+++ cse3902/Entities/IEntintyStateMachine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace cse3902.Entities {
+
+    interface IEntintyStateMachine {
+
+        void Attack();
+        void ChangeDir();
+        void SwitchItem();
+        void TakeDamage();
+        Boolean IsDead();
+        void Update();
+    }
+}
