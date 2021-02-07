27ab5da982fbd2b61498657a61f7d19b723ed2c1
Dev <dvptl68@gmail.com>
2021-02-07 15:13:24 -0500

    Update state machine interface

--- cse3902/Interfaces/IEntityStateMachine.cs
+++ cse3902/Interfaces/IEntityStateMachine.cs
@@ -4,6 +4,10 @@ namespace cse3902.Entities {
 
     public interface IEntityStateMachine {
 
-        
+        public void FaceUp();
+        public void FaceDown();
+        public void FaceLeft();
+        public void FaceRight();
+        public void CycleWeapon();
     }
 }
