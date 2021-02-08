e14b898f07833526a5b4249e931ee6b0ed180887
smerapala <smerapala@gmail.com>
2021-02-08 10:41:03 -0500

    Added ItemHandler class and Item Command classes.

--- cse3902/Game1.cs
+++ cse3902/Game1.cs
@@ -21,9 +21,7 @@ namespace cse3902
         public List<ISprite> spriteList { get; set; }
         List<IController> controllerList;
 
-        public List<ISprite> items;
-        private int itemIndex = 0;
-        private int itemDuration = 0;
+        private ItemHandler itemHandler;
 
         private ISprite arrow;
         private ISprite bomb;
@@ -49,14 +47,12 @@ namespace cse3902
             controllerList.Add(new KeyboardController(this));
             controllerList.Add(new MouseController(this));
 
-            items = new List<ISprite>();
+            itemHandler = new ItemHandler();
 
             // Initialize sprite list
             spriteList = new List<ISprite>();
             this.IsMouseVisible = true;
 	        base.Initialize();
-
-           
         }
 
         /// <summary>
@@ -80,37 +76,7 @@ namespace cse3902
 	        // The sprite index should initially be out of bounds so that there are no premature calls
             currentSpriteIndex = spriteList.Count;
 
-
-            //Texture2D arrowSprite = this.Content.Load<Texture2D>("arrow");
-            //Texture2D bombExplode = this.Content.Load<Texture2D>("bombexplode");
-
-            items.Add(new ArrowItem(spriteBatch, this.Content.Load<Texture2D>("arrow"), new Vector2(100, 100)));
-            items.Add(new BombItem(spriteBatch, this.Content.Load<Texture2D>("bombnew"), new Vector2(100, 100)));
-            items.Add(new BoomerangItem(spriteBatch, this.Content.Load<Texture2D>("boomerang"), new Vector2(100, 100)));
-            items.Add(new BowItem(spriteBatch, this.Content.Load<Texture2D>("bow"), new Vector2(100, 100)));
-            items.Add(new ClockItem(spriteBatch, this.Content.Load<Texture2D>("clock"), new Vector2(100, 100)));
-            items.Add(new FairyItem(spriteBatch, this.Content.Load<Texture2D>("fairy"), new Vector2(100, 100)));
-            items.Add(new CompassItem(spriteBatch, this.Content.Load<Texture2D>("compass"), new Vector2(100, 100)));
-            items.Add(new HeartItem(spriteBatch, this.Content.Load<Texture2D>("heart"), new Vector2(100, 100)));
-            items.Add(new HeartContainerItem(spriteBatch, this.Content.Load<Texture2D>("heartcont"), new Vector2(100, 100)));
-            items.Add(new TriforceItem(spriteBatch, this.Content.Load<Texture2D>("triforce"), new Vector2(100, 100)));
-            items.Add(new KeyItem(spriteBatch, this.Content.Load<Texture2D>("key"), new Vector2(100, 100)));
-            items.Add(new MapItem(spriteBatch, this.Content.Load<Texture2D>("map"), new Vector2(100, 100)));
-
-
-            foreach (ISprite item in items)
-            {
-                item.StartingPosition = new Vector2(100, 100);
-            }
-
-            
-            //arrow = new ArrowItem(spriteBatch, arrowSprite, new Vector2(100, 100));
-            //arrow.StartingPosition = new Vector2(100, 100);
-
-            //Texture2D bombExplode = this.Content.Load<Texture2D>("bombexplode");
-            //bomb = new BombItem(spriteBatch, bombExplode, new Vector2(100, 100));
-            //bomb.StartingPosition = new Vector2(200, 100);
-
+            itemHandler.LoadContent(spriteBatch, Content);
         }
 
         /// <summary>
@@ -141,23 +107,7 @@ namespace cse3902
                 spriteList[currentSpriteIndex].Update(gameTime);
             }
 
-            if (itemDuration < 100)
-            {
-                items[itemIndex].Update(gameTime);
-                itemDuration++;
-            }
-            else
-            {
-                itemDuration = 0;
-                itemIndex++;
-                if (itemIndex >= items.Count)
-                {
-                    itemIndex = 0;
-                }
-            }
-
-            //arrow.Update(gameTime);
-            //bomb.Update(gameTime);
+            itemHandler.Update(gameTime);
 
             base.Update(gameTime);
         }
@@ -176,10 +126,7 @@ namespace cse3902
             }
             textSprite.Draw();
 
-            items[itemIndex].Draw();
-
-            //arrow.Draw();
-            //bomb.Draw();
+            itemHandler.Draw();
 
             base.Draw(gameTime);
         }
