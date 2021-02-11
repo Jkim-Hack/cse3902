b31c0019cf37269c24cd4898c33e4a96e1ac9136
Alex Book <book.45@osu.edu>
2021-02-11 12:02:59 -0500

    added skeletons for aquamentus class and sprite

---
+++ cse3902/Entities/Enemies/Aquamentus.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using cse3902.Sprites.EnemySprites;
+
+namespace cse3902.Entities.Enemies
+{
+    public class Aquamentus: IEntity
+    {
+        private AquamentusSprite aquamentusSprite;
+
+        public Aquamentus()
+        {
+        }
+
+        public Rectangle Bounds
+        {
+            get => throw new NotImplementedException();
+            //get => aquamentusSprite.Texture.Bounds;
+        }
+        public void Attack()
+        {
+
+        }
+        public void ChangeDirection(Vector2 direction)
+        {
+
+        }
+        public void TakeDamage()
+        {
+
+        }
+        public void Die()
+        {
+
+        }
+        public void Update(GameTime gameTime)
+        {
+
+        }
+    }
+}
---
+++ cse3902/Sprites/EnemySprites/AquamentusSprite.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using cse3902.Interfaces;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace cse3902.Sprites.EnemySprites
+{
+    public class AquamentusSprite: ISprite
+    {
+        public AquamentusSprite()
+        {
+        }
+
+        public Vector2 StartingPosition { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public Vector2 Center { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public Texture2D Texture => throw new NotImplementedException();
+
+        public void Draw()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Erase()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
