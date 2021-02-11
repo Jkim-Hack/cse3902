d1d2b00731d4169b9ddc5fab30ee1f3d664c058b
Alex Book <book.45@osu.edu>
2021-02-10 21:08:25 -0500

    ignore this commit

--- cse3902/Entities/NPCStateMachine.cs
+++ cse3902/Entities/NPCStateMachine.cs
@@ -8,13 +8,13 @@ namespace cse3902.Entities
     {
         public void CycleWeapon(int dir)
         {
-            //Enemies and npcs don't change weapons
+            //Npcs don't change weapons
             throw new NotImplementedException();
         }
 
         public void ChangeDirection(Vector2 newDirection)
         {
-            //will implement after enemy spriting is done
+            //TODO: will implement after npc spriting is done
         }
     }
 }
