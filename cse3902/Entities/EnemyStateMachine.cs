81b6fd5bf7372a0bc6fb7b2f17b64e63c416e690
Alex Book <book.45@osu.edu>
2021-02-10 20:53:14 -0500

    renamed to enemystatemachine

--- cse3902/Entities/EnemyStateMachine.cs
+++ cse3902/Entities/EnemyStateMachine.cs
@@ -3,16 +3,17 @@ using Microsoft.Xna.Framework;
 
 namespace cse3902.Entities {
 
-    public class EnemyDragonStateMachine : IEntityStateMachine
+    public class EnemyStateMachine : IEntityStateMachine
     {
         public void CycleWeapon(int dir)
         {
+            //Enemies and npcs don't change weapons
             throw new NotImplementedException();
         }
 
         public void ChangeDirection(Vector2 newDirection)
         {
-
+            //will implement after enemy spriting is done
         }
     }
 }
