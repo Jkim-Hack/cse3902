65f14db1733d47521a9e968ff9da454b86c99695
Alex Book <book.45@osu.edu>
2021-02-16 17:07:16 -0500

    gel classes

---
+++ cse3902/Entities/GelStateMachine.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using cse3902.Sprites.EnemySprites;
+
+
+namespace cse3902.Entities
+{
+    public class GelStateMachine
+    {
+        private GelSprite gelSprite;
+
+
+
+        public GelStateMachine(GelSprite stalfosSprite)
+        {
+            this.gelSprite = gelSprite;
+
+        }
+
+        public void CycleWeapon(int dir)
+        {
+            //Enemies don't change weapons
+            throw new NotImplementedException();
+        }
+
+        public void ChangeDirection(Vector2 newDirection)
+        {
+           
+
+
+        }
+
+
+        public void TakeDamage()
+        {
+
+        }
+
+        public void Attack()
+        {
+            gelSprite.IsAttacking = true;
+        }
+    }
+}
