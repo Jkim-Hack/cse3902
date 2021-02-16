0d89202bb523211070bc1b669f4e6c133bda1977
Alex Book <book.45@osu.edu>
2021-02-15 22:42:58 -0500

    added stalfos and wall master code

---
+++ cse3902/Entities/Enemies/WallMaster.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace cse3902.Entities.Enemies
+{
+    public class WallMaster
+    {
+        public WallMaster()
+        {
+        }
+    }
+}
---
+++ cse3902/Entities/WallMasterStateMachine.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace cse3902.Entities
+{
+    public class WallMasterStateMachine
+    {
+        public WallMasterStateMachine()
+        {
+        }
+    }
+}
