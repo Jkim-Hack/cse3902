1482e33e894649f40d1cc4ba3a9f4faabfc0184a
Dev <dvptl68@gmail.com>
2021-02-22 11:55:42 -0500

    Complete wallmaster movement

--- cse3902/Entities/Enemies/WallMaster.cs
+++ cse3902/Entities/Enemies/WallMaster.cs
@@ -13,23 +13,23 @@ namespace cse3902.Entities.Enemies
 
         private Vector2 direction;
         private float speed;
-        private Vector2 startingPos;
         private Vector2 center;
-        private int travelDistance;
+        private float range;
+        private float traveled;
 
         public WallMaster(Game1 game)
         {
             this.game = game;
             Texture2D wallMasterTexture = game.Content.Load<Texture2D>("enemies/wall_master");
-            startingPos = new Vector2(500, 200);
-            center = startingPos;
+            center = new Vector2(500, 200);
 
             //wallmaster sprite sheet is 4 rows, 2 columns
             wallMasterSprite = new WallMasterSprite(game.spriteBatch, wallMasterTexture, 4, 2, center);
             wallMasterStateMachine = new WallMasterStateMachine(wallMasterSprite);
             direction = new Vector2(-1, 1);
-            speed = 50.0f;
-            travelDistance = 80;
+            speed = 6.5f;
+            range = 15;
+            traveled = range;
         }
 
         public Rectangle Bounds
@@ -59,24 +59,32 @@ namespace cse3902.Entities.Enemies
         }
         public void Update(GameTime gameTime)
         {
-            this.CenterPosition += direction * speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            var change = speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
-            if (direction.X > 0 && direction.Y > 0 && CenterPosition.Y > startingPos.Y + travelDistance) {
+            traveled -= change;
+            if (traveled <= 0) {
 
-                direction.Y = -1;
+                if (direction.X > 0 && direction.Y > 0) {
 
-            } else if (direction.X > 0 && direction.Y < 0 && CenterPosition.X > startingPos.X + travelDistance) {
+                    direction.Y = -1;
 
-                direction.X = -1;
+                } else if (direction.X > 0 && direction.Y < 0) {
 
-            } else if (direction.X < 0 && direction.Y > 0 && CenterPosition.X < startingPos.X - travelDistance) {
+                    direction.X = -1;
 
-                direction.X = 1;
+                } else if (direction.X < 0 && direction.Y > 0) {
 
-            } else if (direction.X < 0 && direction.Y < 0 && CenterPosition.Y < startingPos.Y - travelDistance) {
+                    direction.X = 1;
 
-                direction.Y = 1;
+                } else if (direction.X < 0 && direction.Y < 0) {
+
+                    direction.Y = 1;
+                }
+
+                traveled = range;
             }
+
+            this.CenterPosition += direction * speed * change;
 
             ChangeDirection(direction);
             wallMasterSprite.Update(gameTime, onSpriteAnimationComplete);
