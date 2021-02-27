531585035db2bf450f3d0f2c9490888739fc36b9
smerapala <smerapala@gmail.com>
2021-02-26 21:29:30 -0500

    Add room class and room handler

---
+++ cse3902/Rooms/Room.cs
@@ -0,0 +1,49 @@
+﻿using cse3902.Interfaces;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using cse3902.Entities;
+
+namespace cse3902.Rooms
+{
+    public class Room
+    {
+        //private SpriteBatch spriteBatch;
+
+        private Vector2 topLeft;
+        private Vector2 bottomRight;
+        private bool visited;
+        private int[] surrounding;
+
+        private List<IItem> items;
+        private List<IEntity> enemies;
+        private List<IProjectile> projectiles;
+
+        public Room(int top, int right, int bottom, int left)
+        {
+            surrounding[0] = top;
+            surrounding[1] = right;
+            surrounding[2] = bottom;
+            surrounding[3] = left;
+
+            visited = false;
+
+
+        }
+
+        public void AddItem (IItem item)
+        {
+            items.Add(item);
+        }
+
+        public void AddEnemy(IEntity enemy)
+        {
+            enemies.Add(enemy);
+        }
+
+        public void AddProjectile(IProjectile projectile)
+        {
+            projectiles.Add(projectile);
+        }
+    }
+}
---
+++ cse3902/Rooms/RoomHandler.cs
@@ -0,0 +1,42 @@
+﻿using cse3902.Interfaces;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using cse3902.Entities;
+using System;
+
+namespace cse3902.Rooms
+{
+    public class RoomHandler
+    {
+        private List<Room> rooms;
+
+        private int roomIndex = 0;
+
+        public RoomHandler()
+        {
+
+
+
+        }
+
+        public void CycleNext()
+        {
+            roomIndex++;
+            if (roomIndex >= rooms.Count)
+            {
+                roomIndex = 0;
+            }
+             
+        }
+
+        public void CyclePrev()
+        {
+            roomIndex--;
+            if (roomIndex < 0)
+            {
+                roomIndex = rooms.Count - 1;
+            }
+        }
+    }
+}
