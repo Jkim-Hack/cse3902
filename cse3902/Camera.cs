040735d7aceb219bc2476e46485d5981a1807be4
Pranav <koduri.1@napls.us>
2021-03-01 16:06:15 -0500

    cleaned up camera class a bit and added a new method

--- cse3902/Camera.cs
+++ cse3902/Camera.cs
@@ -26,9 +26,9 @@ namespace cse3902
             windowBounds = new Vector2(game.Window.ClientBounds.Width, game.Window.ClientBounds.Height);
 
             topLeftCoordinate = new Vector2(0, 0);
-            dimensionScale = new Vector2(1,1);
+            dimensionScale = new Vector2(1, 1);
 
-            transformationMatrix = Matrix.CreateScale(new Vector3(dimensionScale,0));
+            transformationMatrix = Matrix.CreateScale(new Vector3(dimensionScale, 0));
 
             cameraIsMoving = false;
             smoothMovementDirection = new Vector2(0, 0);
@@ -38,7 +38,12 @@ namespace cse3902
 
         public void MoveCamera(Vector2 topLeft, int cameraWidth, int cameraHeight)
         {
-            if (!cameraIsMoving) MoveCamera(topLeft, new Vector2(cameraWidth,cameraHeight));
+            MoveCamera(topLeft, new Vector2(cameraWidth, cameraHeight));
+        }
+
+        public void MoveCamera(Rectangle newCamera)
+        {
+            MoveCamera(newCamera.Location.ToVector2(), newCamera.Size.ToVector2());
         }
 
         public void MoveCamera(Vector2 topLeft, Vector2 dimensions)
@@ -66,22 +71,22 @@ namespace cse3902
 
         public void MoveCameraUp(int pixels)
         {
-            if (!cameraIsMoving) MoveCamera(new Vector2(0, -pixels));
+            MoveCamera(new Vector2(0, -pixels));
         }
 
         public void MoveCameraDown(int pixels)
         {
-            if (!cameraIsMoving) MoveCamera(new Vector2(0, pixels));
+            MoveCamera(new Vector2(0, pixels));
         }
 
         public void MoveCameraLeft(int pixels)
         {
-            if (!cameraIsMoving) MoveCamera(new Vector2(-pixels, 0));
+            MoveCamera(new Vector2(-pixels, 0));
         }
 
         public void MoveCameraRight(int pixels)
         {
-            if (!cameraIsMoving) MoveCamera(new Vector2(pixels, 0));
+            MoveCamera(new Vector2(pixels, 0));
         }
 
         public Matrix GetTransformationMatrix()
@@ -97,7 +102,7 @@ namespace cse3902
                 smoothMovementDestination = topLeftCoordinate + translation;
 
                 if (numberUpdateCyclesToComplete < 1) numberUpdateCyclesToComplete = 1;
-                smoothMovementDirection = translation / ((float) numberUpdateCyclesToComplete);
+                smoothMovementDirection = translation / ((float)numberUpdateCyclesToComplete);
 
                 cameraIsMoving = true;
             }
