75f222e4fbd5d0462eaaf9e64cd1736d25977c47
Alex Book <book.45@osu.edu>
2021-03-07 16:12:36 -0500

    add collidable member to IProjectile

--- cse3902/Interfaces/IProjectile.cs
+++ cse3902/Interfaces/IProjectile.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using cse3902.Collision;
 namespace cse3902.Interfaces
 {
     public interface IProjectile: ISprite
     {
         public bool AnimationComplete { get; set; }
+        public ICollidable Collidable { get; }
     }
 }
