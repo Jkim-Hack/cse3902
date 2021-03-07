3049d3950d0a81d041e3b6f81444920bb7488e50
Alex Book <book.45@osu.edu>
2021-03-07 10:22:06 -0500

    add collidable member to stalfos

--- cse3902/Entities/Enemies/Stalfos.cs
+++ cse3902/Entities/Enemies/Stalfos.cs
@@ -1,4 +1,6 @@
 ﻿using cse3902.Interfaces;
+using cse3902.Collision;
+using cse3902.Collision.Collidables;
 using cse3902.SpriteFactory;
 using cse3902.Sprites.EnemySprites;
 using Microsoft.Xna.Framework;
@@ -20,6 +22,8 @@ namespace cse3902.Entities.Enemies
         private Vector2 shoveDirection;
         private int shoveDistance;
 
+        private ICollidable collidable;
+
         public Stalfos(Game1 game)
         {
             this.game = game;
@@ -33,6 +37,8 @@ namespace cse3902.Entities.Enemies
             speed = 50.0f;
             travelDistance = 80;
             shoveDistance = -10;
+
+            this.collidable = new EnemyCollidable(this, this.Damage);
         }
 
         public ref Rectangle Bounds
@@ -108,6 +114,16 @@ namespace cse3902.Entities.Enemies
                 this.center = value;
                 stalfosSprite.Center = value;
             }
+        }
+
+        public int Damage
+        {
+            get => 2;
+        }
+
+        public ICollidable Collidable
+        {
+            get => this.collidable;
         }
     }
 }
