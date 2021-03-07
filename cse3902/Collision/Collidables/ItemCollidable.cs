0775ed4e5e83218d48ab4049db11d571014d41ab
Alex Book <book.45@osu.edu>
2021-03-07 18:11:01 -0500

    add itemcollidable class

---
+++ cse3902/Collision/Collidables/ItemCollidable.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using cse3902.Interfaces;
+using Microsoft.Xna.Framework;
+
+namespace cse3902.Collision.Collidables
+{
+    public class ItemCollidable : ICollidable
+    {
+        private IItem item;
+
+
+        public ItemCollidable(IItem item)
+        {
+            this.item = item;
+        }
+
+
+        public void OnCollidedWith(ICollidable collidableObject)
+        {
+
+            if (collidableObject is PlayerCollidable)
+            {
+                //player needs to pick up item
+            }
+            else
+            {
+                //do nothing
+            }
+
+
+        }
+
+        public ref Rectangle RectangleRef
+        {
+            get => ref item.Box;
+        }
+
+        public int DamageValue
+        {
+            get => 0;
+        }
+    }
+}
