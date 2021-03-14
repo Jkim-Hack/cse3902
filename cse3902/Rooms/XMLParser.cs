9d44be3672efc82ab8b5320e8781f899afd38ed9
smerapala <smerapala@gmail.com>
2021-03-14 17:03:40 -0400

    Added methods for linking doors

--- cse3902/Rooms/XMLParser.cs
+++ cse3902/Rooms/XMLParser.cs
@@ -10,6 +10,7 @@ using cse3902.SpriteFactory;
 using cse3902.Entities.Enemies;
 using cse3902.Entities;
 using cse3902.Blocks;
+using cse3902.Doors;
 using System.Linq;
 
 namespace cse3902.Rooms
@@ -122,12 +123,31 @@ namespace cse3902.Rooms
             return newBlock;
         }
 
-        public IDoor createDoor(String type, String dir, Vector2 startingPos)
+        public IDoor createDoor(String type, Vector2 startingPos)
         {
             IDoor newDoor = null;
             switch (type)
             {
-
+                case "Up":
+                    newDoor = new NormalUpDoor(game, startingPos, IDoor.DoorState.Open);
+                    break;
+                case "Down":
+                    newDoor = new NormalDownDoor(game, startingPos, IDoor.DoorState.Open);
+                    break;
+                case "Left":
+                    newDoor = new NormalLeftDoor(game, startingPos, IDoor.DoorState.Open);
+                    break;
+                case "Right":
+                    newDoor = new NormalRightDoor(game, startingPos, IDoor.DoorState.Open);
+                    break;
+                case "StairDown":
+                    newDoor = new DownStaircaseDoor(game, startingPos);
+                    break;
+                case "OffscreenUp":
+                    newDoor = new OffscreenUpDoor(game, startingPos);
+                    break;
+                default:
+                    break;
             }
 
             return newDoor;
@@ -210,16 +230,26 @@ namespace cse3902.Rooms
             foreach (XElement door in doorList)
             {
                 XElement typeName = door.Element("type");
-                XElement dir = door.Element("dir");
+                XElement connRoom = door.Element("connRoom");
                 XElement xloc = door.Element("xloc");
                 XElement yloc = door.Element("yloc");
 
                 int x = Int32.Parse(xloc.Value);
                 int y = Int32.Parse(yloc.Value);
 
-                IDoor doorAdd = createDoor(typeName.Value, dir.Value, new Vector2(x, y));
+                IDoor doorAdd = createDoor(typeName.Value, new Vector2(x, y));
                 roomobj.AddDoor(doorAdd);
             }
+
+        }
+
+        private void LinkDoors()
+        {
+
+        }
+
+        private void HandleDoorConnection(Vector3 currRoom)
+        {
 
         }
 
