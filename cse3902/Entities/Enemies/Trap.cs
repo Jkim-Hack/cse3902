5c333a473189b8b302dbf043b2e78a3e9b8836b3
Alex Book <book.45@osu.edu>
2021-03-15 13:40:53 -0400

    add trap to enemyspritefactory

--- cse3902/Entities/Enemies/Trap.cs
+++ cse3902/Entities/Enemies/Trap.cs
@@ -1,10 +1,42 @@
 ﻿using System;
+using cse3902.Interfaces;
+using Microsoft.Xna.Framework;
+using cse3902.Sprites.EnemySprites;
+
 namespace cse3902.Entities.Enemies
 {
-    public class Trap
+    public class Trap: IEntity
     {
-        public Trap()
+        private TrapSprite trapSprite;
+        private readonly Game1 game;
+
+        private Vector2 direction;
+        private float speed;
+        private Vector2 startingPos;
+        private Vector2 center;
+        private int travelDistance;
+        private Vector2 shoveDirection;
+        private int shoveDistance;
+
+        private ICollidable collidable;
+        private int health;
+
+        public Trap(Game1 game, Vector2 start)
         {
+            this.game = game;
+            startingPos = start;
+            center = startingPos;
+
+            //gel sprite sheet is 1 row, 2 columns
+            gelSprite = (GelSprite)EnemySpriteFactory.Instance.CreateGelSprite(game.spriteBatch, startingPos);
+            gelStateMachine = new GelStateMachine(gelSprite);
+            direction = new Vector2(-1, 0);
+            speed = 50.0f;
+            travelDistance = 50;
+            shoveDistance = -10;
+
+            this.collidable = new EnemyCollidable(this, this.Damage);
+            health = 2;
         }
     }
 }
--- cse3902/Sprites/EnemySprites/TrapSprite.cs
+++ cse3902/Sprites/EnemySprites/TrapSprite.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using cse3902.Interfaces;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
 namespace cse3902.Sprites.EnemySprites
 {
     public class TrapSprite
     {
-        public TrapSprite()
+        public TrapSprite(SpriteBatch spriteBatch, Texture2D texture, int rows, int columns, Vector2 startingPosition)
         {
         }
     }
