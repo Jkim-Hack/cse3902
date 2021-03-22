ea6cc311605747f93064fa38416b5b4f11771d0b
John Kim <johnkim1108@gmail.com>
2021-03-22 16:46:57 -0400

    Add HUDManager

---
+++ cse3902/HUD/HUDManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using cse3902.Interfaces;
+
+namespace cse3902.HUD
+{
+    public class HUDManager
+    {
+        public enum HUDItemKey 
+	    { 
+	        INVENTORY,
+            MAP_ITEM,
+            COMPASS_ITEM,
+            YELLOW_MAP,
+            MINIMAP,
+	        COLLECTABLES,
+            B_ITEM,
+            A_ITEM,
+            HEALTH
+	    }
+	
+	    private Game1 game;
+        private Dictionary<HUDItemKey, IHUDItem> HUDItems;
+
+        public HUDManager(Game1 game)
+        {
+            this.game = game;
+            HUDItems = new Dictionary<HUDItemKey, IHUDItem>();
+        }
+    }
+}
