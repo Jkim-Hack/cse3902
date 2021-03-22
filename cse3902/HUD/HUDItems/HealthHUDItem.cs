4795a85ee474985bcef54d2655b63abe9a8d53d5
John Kim <johnkim1108@gmail.com>
2021-03-22 17:10:31 -0400

    Add HealthHUDItem

---
+++ cse3902/HUD/HUDItems/HealthHUDItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace cse3902.HUD.HUDItems
+{
+    public class HealthHUDItem : IHUDItem
+    {
+        public HealthHUDItem()
+        {
+        }
+
+        public Vector2 Center { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public Texture2D Texture => throw new NotImplementedException();
+
+        public ref Rectangle Box => throw new NotImplementedException();
+
+        public void Draw()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Erase()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
