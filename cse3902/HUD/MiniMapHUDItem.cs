08ac8838fe5e293bf69888c4acfa29be585fd3b6
Dev <dvptl68@gmail.com>
2021-03-24 10:26:02 -0400

    Add HUD items

---
+++ cse3902/HUD/MiniMapHUDItem.cs
@@ -0,0 +1,24 @@
+using cse3902.Interfaces;
+using Microsoft.Xna.Framework;
+
+namespace cse3902.HUD
+{
+    public class MiniMapHUDItem
+    {
+
+        public MiniMapHUDItem()
+        {
+
+        }
+
+        public void Update()
+        {
+            
+        }
+
+        public void Draw()
+        {
+            
+        }
+    }
+}
