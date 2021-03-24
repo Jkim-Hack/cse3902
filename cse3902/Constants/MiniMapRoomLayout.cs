c8802ec5360074cad2a1d231d07567b91971409b
Dev <dvptl68@gmail.com>
2021-03-24 17:18:47 -0400

    Create minimap constants class

---
+++ cse3902/Constants/MiniMapRoomLayout.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Immutable;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace cse3902.Constants
+{
+    public class MiniMapRoomLayout
+    {
+        public ImmutableList<Rectangle> RoomLayout = ImmutableList.Create<Rectangle>
+        (
+            
+        );
+    }
+}
--- cse3902/HUD/MiniMapHUDItem.cs
+++ cse3902/HUD/MiniMapHUDItem.cs
@@ -1,10 +1,14 @@
 using cse3902.Interfaces;
+using cse3902.Constants;
 using Microsoft.Xna.Framework;
 
 namespace cse3902.HUD
 {
     public class MiniMapHUDItem
     {
+
+        private Game1 game;
+        private Rectangle currentRoom;
 
         public MiniMapHUDItem()
         {
