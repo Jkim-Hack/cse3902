f8baa7144e111e4f85e1484459903d9398850c3d
Dev <dvptl68@gmail.com>
2021-03-24 18:27:18 -0400

    Add drawing functionality with test offsets

--- cse3902/Constants/MiniMapRoomLayout.cs
+++ cse3902/Constants/MiniMapRoomLayout.cs
@@ -6,10 +6,12 @@ namespace cse3902.Constants
 {
     public class MiniMapRoomLayout
     {
+        public const int offsetX = 100;
+        public const int offsetY = 500;
 
         public static ReadOnlyCollection<Rectangle> RoomLayout = new ReadOnlyCollection<Rectangle>(new List<Rectangle>
         {
-            new Rectangle(0, 0, 10, 10)
+            new Rectangle(0, 0, 50, 50)
         });
     }
 }
--- cse3902/HUD/MiniMapHUDItem.cs
+++ cse3902/HUD/MiniMapHUDItem.cs
@@ -1,6 +1,7 @@
 using cse3902.Interfaces;
 using cse3902.Constants;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 
 namespace cse3902.HUD
@@ -14,6 +15,7 @@ namespace cse3902.HUD
         public MiniMapHUDItem(Game1 game)
         {
             this.game = game;
+            this.currentRoom = new Rectangle(40 + MiniMapRoomLayout.offsetX, 40 + MiniMapRoomLayout.offsetY, 50, 50);
         }
 
         public void Update()
@@ -23,10 +25,20 @@ namespace cse3902.HUD
 
         public void Draw()
         {
+            /* Draw whole map first, then current room */
             foreach(Rectangle rec in MiniMapRoomLayout.RoomLayout)
             {
-                Console.WriteLine(rec);
+                DrawRectangle(new Rectangle(rec.X + MiniMapRoomLayout.offsetX, rec.Y + MiniMapRoomLayout.offsetY, rec.Width, rec.Height), Color.RoyalBlue);
             }
+            DrawRectangle(currentRoom, Color.LimeGreen);
+        }
+
+        private void DrawRectangle(Rectangle rectangle, Color color)
+        {
+            Texture2D txt = new Texture2D(game.GraphicsDevice, 1, 1);
+            txt.SetData(new Color[] { Color.White });
+
+            game.SpriteBatch.Draw(txt, rectangle, color);
         }
     }
 }
