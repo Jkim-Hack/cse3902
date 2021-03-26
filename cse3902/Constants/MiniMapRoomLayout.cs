05afe1e8a05786371771bdeecd6c79e8dcac5ccb
Dev <dvptl68@gmail.com>
2021-03-26 16:15:26 -0400

    Update algorithm for room placement

--- cse3902/Constants/MiniMapRoomLayout.cs
+++ cse3902/Constants/MiniMapRoomLayout.cs
@@ -6,11 +6,22 @@ namespace cse3902.Constants
     public class MiniMapRoomLayout
     {
         public const int offsetX = 100;
-        public const int offsetY = 500;
+        public const int offsetY = 600;
 
+        public const int currentRoomSize = 8;
+
+        /* Real aspect ratio of blue minimap boxes is 7:3, with a gap of 2 */
+        private const int width = 16;
+        private const int height = 8;
+        private const int gap = 3;
+
+        /* Can be thought of as a "coordinate system", with the starting room at (0, 0) */
         public static List<Rectangle> RoomLayout = new List<Rectangle>
         {
-            new Rectangle(0, 0, 50, 50)
+            new Rectangle(0 * (width + gap), 0 * (height + gap) * -1, width, height),
+            new Rectangle(-1 * (width + gap), 0 * (height + gap) * -1, width, height),
+            new Rectangle(1 * (width + gap), 0 * (height + gap) * -1, width, height),
+            new Rectangle(0 * (width + gap), 1 * (height + gap) * -1, width, height)
         };
     }
 }
--- cse3902/HUD/MiniMapHUDItem.cs
+++ cse3902/HUD/MiniMapHUDItem.cs
@@ -15,7 +15,7 @@ namespace cse3902.HUD
         public MiniMapHUDItem(Game1 game)
         {
             this.game = game;
-            this.currentRoom = new Rectangle(40 + MiniMapRoomLayout.offsetX, 40 + MiniMapRoomLayout.offsetY, 50, 50);
+            this.currentRoom = new Rectangle(0, 0, 0, 0);
         }
 
         public void Update()
@@ -25,23 +25,20 @@ namespace cse3902.HUD
 
         public void Draw()
         {
-            /* Draw background */
-            DrawRectangle(new Rectangle(0, 0, game.GraphicsDevice.Viewport.Width, game.GraphicsDevice.Viewport.Height), Color.Black);
+            /* Draw background (just for testing) */
+            DrawRectangle(new Rectangle(0 - MiniMapRoomLayout.offsetX, 0 - MiniMapRoomLayout.offsetY, game.GraphicsDevice.Viewport.Width, game.GraphicsDevice.Viewport.Height), Color.Black);
 
             /* Draw whole map first, then current room */
-            foreach(Rectangle rec in MiniMapRoomLayout.RoomLayout)
-            {
-                DrawRectangle(new Rectangle(rec.X + MiniMapRoomLayout.offsetX, rec.Y + MiniMapRoomLayout.offsetY, rec.Width, rec.Height), Color.RoyalBlue);
-            }
+            foreach(Rectangle rec in MiniMapRoomLayout.RoomLayout) DrawRectangle(rec, Color.RoyalBlue);
             DrawRectangle(currentRoom, Color.LimeGreen);
         }
 
-        private void DrawRectangle(Rectangle rectangle, Color color)
+        private void DrawRectangle(Rectangle rec, Color color)
         {
-            Texture2D txt = new Texture2D(game.GraphicsDevice, 1, 1);
-            txt.SetData(new Color[] { Color.White });
+            Texture2D texture = new Texture2D(game.GraphicsDevice, 1, 1);
+            texture.SetData(new Color[] { Color.White });
 
-            game.SpriteBatch.Draw(txt, rectangle, color);
+            game.SpriteBatch.Draw(texture, new Rectangle(rec.X + MiniMapRoomLayout.offsetX, rec.Y + MiniMapRoomLayout.offsetY, rec.Width, rec.Height), color);
         }
     }
 }
