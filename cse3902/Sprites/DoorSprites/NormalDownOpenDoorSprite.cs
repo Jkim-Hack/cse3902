dd53596cfabef5af8de5735f580be71238c53ce1
Pranav <koduri.1@napls.us>
2021-03-28 02:22:56 -0400

    bombed door sprite stuff works now

--- cse3902/Sprites/DoorSprites/NormalDownOpenDoorSprite.cs
+++ cse3902/Sprites/DoorSprites/NormalDownOpenDoorSprite.cs
@@ -8,15 +8,17 @@ namespace cse3902.Sprites
     {
         private SpriteBatch spriteBatch;
         private Texture2D spriteTexture;
+        private Texture2D blackTexture;
         private Vector2 center;
-
 
         private Rectangle dest;
         private Rectangle door;
 
+        private bool bombedDoor;
+
         private const float sizeIncrease = 1f;
 
-        public NormalDownOpenDoorSprite(SpriteBatch spriteBatch, Texture2D texture, Vector2 startingPosition, Rectangle wantedDoor)
+        public NormalDownOpenDoorSprite(SpriteBatch spriteBatch, Texture2D blackTexture, Texture2D texture, Vector2 startingPosition, Rectangle wantedDoor, bool bombed)
         {
             this.spriteBatch = spriteBatch;
             spriteTexture = texture;
@@ -24,12 +26,15 @@ namespace cse3902.Sprites
             center = startingPosition;
 
             door = wantedDoor;
+            bombedDoor = bombed;
+            this.blackTexture = blackTexture;
         }
 
         public void Draw()
         {
             Vector2 origin = new Vector2(door.Width / 2f, door.Height / 2f);
             Rectangle Destination = new Rectangle((int)center.X, (int)center.Y, (int)(door.Width * sizeIncrease), (int)(door.Height * sizeIncrease));
+            if (bombedDoor) spriteBatch.Draw(blackTexture, Destination, null, Color.White, 0, origin, SpriteEffects.None, SpriteUtilities.BombedDoorBackgroundLayer);
             spriteBatch.Draw(spriteTexture, Destination, door, Color.White, 0, origin, SpriteEffects.None, SpriteUtilities.TopBackgroundLayer);
         }
 
