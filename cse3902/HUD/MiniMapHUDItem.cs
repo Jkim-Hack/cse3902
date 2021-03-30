b025dfa793583709f82b0ebd4a3bb2605e803f1f
Dev <dvptl68@gmail.com>
2021-03-30 13:19:13 -0400

    Update minimap hud item to implement interface

--- cse3902/HUD/MiniMapHUDItem.cs
+++ cse3902/HUD/MiniMapHUDItem.cs
@@ -3,11 +3,10 @@ using cse3902.Constants;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
-using cse3902.Utilities;
 
 namespace cse3902.HUD
 {
-    public class MiniMapHUDItem
+    public class MiniMapHUDItem : IHUDItem
     {
         private Game1 game;
 
@@ -21,6 +20,8 @@ namespace cse3902.HUD
 
         private bool alreadyChanged;
 
+        private Rectangle box; // placeholder since this HUD item doesn't have a real rectangle
+
         public MiniMapHUDItem(Game1 game)
         {
             this.game = game;
@@ -32,9 +33,11 @@ namespace cse3902.HUD
             this.alreadyChanged = false;
             colorRed = false;
             delay = MiniMapConstants.COLOR_DELAY;
+
+            box = new Rectangle();
         }
 
-        public void Update()
+        public int Update(GameTime gameTime)
         {
             if (game.RoomHandler.roomTransitionManager.IsTransitioning() && !alreadyChanged)
             {
@@ -53,14 +56,19 @@ namespace cse3902.HUD
                 delay = MiniMapConstants.COLOR_DELAY;
                 colorRed = !colorRed;
             }
+
+            return 0;
         }
 
         public void Draw()
         {
+            /* Draw background (just for testing) */
+            DrawRectangle(new Rectangle(0 - offsetX, 0 - offsetY, DimensionConstants.OriginalWindowWidth, DimensionConstants.OriginalWindowHeight), Color.Black);
+
             if (InventoryManager.Instance.inventory[InventoryManager.ItemType.Map] > 0)
             {
                 /* Draw entire map */
-                foreach (Rectangle rec in MiniMapConstants.GetRoomLayout()) HUDUtilities.DrawRectangle(game, rec, MiniMapConstants.RoomColor, 0, 0);
+                foreach (Rectangle rec in MiniMapConstants.GetRoomLayout()) DrawRectangle(rec, MiniMapConstants.RoomColor);
             }
 
             if (InventoryManager.Instance.inventory[InventoryManager.ItemType.Compass] > 0)
@@ -68,8 +76,8 @@ namespace cse3902.HUD
                 Rectangle triforceRectangle = MiniMapConstants.CalculatePos((int)MiniMapConstants.TriforcePos.X, (int)MiniMapConstants.TriforcePos.Y);
                 triforceRectangle.X += (MiniMapConstants.Width - MiniMapConstants.Height) / 2;
                 triforceRectangle.Width = MiniMapConstants.Height;
-                if (colorRed) HUDUtilities.DrawRectangle(game, triforceRectangle, MiniMapConstants.TriforceRed, 0, 0);
-                else HUDUtilities.DrawRectangle(game, triforceRectangle, MiniMapConstants.TriforceGreen, 0, 0);
+                if (colorRed) DrawRectangle(triforceRectangle, MiniMapConstants.TriforceRed);
+                else DrawRectangle(triforceRectangle, MiniMapConstants.TriforceGreen);
             }
 
             /* Only draw current room square if not in item room */
@@ -78,8 +86,38 @@ namespace cse3902.HUD
                 Rectangle currentRoomRectangle = MiniMapConstants.CalculatePos((int)currentRoom.X, (int)currentRoom.Y, MiniMapConstants.Width, MiniMapConstants.Height);
                 currentRoomRectangle.X += (MiniMapConstants.Width - MiniMapConstants.Height) / 2;
                 currentRoomRectangle.Width = MiniMapConstants.Height;
-                HUDUtilities.DrawRectangle(game, currentRoomRectangle, MiniMapConstants.CurrentRoomColor, 0, 0);
+                DrawRectangle(currentRoomRectangle, MiniMapConstants.CurrentRoomColor);
             }
+        }
+
+        private void DrawRectangle(Rectangle rec, Color color)
+        {
+            Texture2D texture = new Texture2D(game.GraphicsDevice, 1, 1);
+            texture.SetData(new Color[] { Color.White });
+
+            game.SpriteBatch.Draw(texture, new Rectangle(rec.X + offsetX, rec.Y + offsetY, rec.Width, rec.Height), color);
+        }
+
+        public void Erase() {} // needs to be deleted once isprite is updated
+
+        public Vector2 Center {
+
+            get => new Vector2(offsetX, offsetY);
+
+            set {
+                offsetX = (int) value.X;
+                offsetY = (int) value.Y;
+            }
+        }
+
+        public Texture2D Texture {
+
+            get => null;
+        }
+
+        public ref Rectangle Box {
+
+            get => ref box;
         }
     }
 }
