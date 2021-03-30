3b28fddb8cb51abe7be2563300f44ec7498f1e09
smerapala <smerapala@gmail.com>
2021-03-30 09:31:32 -0500

    Add code for all sounds in sound factory

--- cse3902/Sounds/SoundFactory.cs
+++ cse3902/Sounds/SoundFactory.cs
@@ -16,6 +16,16 @@ namespace cse3902.Sounds
         public SoundEffect enemyDie { get; set; }
         public SoundEffect linkHit { get; set; }
         public SoundEffect linkDie { get; set; }
+        public SoundEffect swordSlash { get; set; }
+        public SoundEffect arrowBoomerang { get; set; }
+        public SoundEffect bombBlow { get; set; }
+        public SoundEffect lowHealth { get; set; }
+        public SoundEffect getItem { get; set; }
+        public SoundEffect text { get; set; }
+        public SoundEffect keyAppear { get; set; }
+        public SoundEffect doorUnlock { get; set; }
+        public SoundEffect stairs { get; set; }
+        public SoundEffect bossScream { get; set; }
 
         private static SoundFactory instance = new SoundFactory();
 
@@ -34,7 +44,21 @@ namespace cse3902.Sounds
         public void LoadAllTextures(ContentManager content)
         {
             getHeart = content.Load<SoundEffect>("LOZ_Get_Heart");
-            enemyHit = content.Load<SoundEffect>("LOZ_Get_Heart");
+            // enemy hurt was unavailable, check later and update sound file
+            enemyHit = content.Load<SoundEffect>("LOZ_Enemy_Die");
+            enemyDie = content.Load<SoundEffect>("LOZ_Enemy_Die");
+            linkHit = content.Load<SoundEffect>("LOZ_Link_Hurt");
+            linkDie = content.Load<SoundEffect>("LOZ_Link_Die");
+            swordSlash = content.Load<SoundEffect>("LOZ_Sword_Slash");
+            arrowBoomerang = content.Load<SoundEffect>("LOZ_Arrow_Boomerang");
+            bombBlow = content.Load<SoundEffect>("LOZ_Bomb_Blow");
+            lowHealth = content.Load<SoundEffect>("LOZ_LowHealth");
+            getItem = content.Load<SoundEffect>("LOZ_Get_Item");
+            text = content.Load<SoundEffect>("LOZ_Text");
+            keyAppear = content.Load<SoundEffect>("LOZ_Key_Appear");
+            doorUnlock = content.Load<SoundEffect>("LOZ_Door_Unlock");
+            stairs = content.Load<SoundEffect>("LOZ_Stairs");
+            bossScream = content.Load<SoundEffect>("LOZ_Boss_Scream1");
         }
 
         public static void PlaySound(SoundEffect sound)
