09f405afb1ed31592ddce8afb6e08eee4eec7eb4
smerapala <smerapala@gmail.com>
2021-03-30 20:59:45 -1000

    Add extra sounds

--- cse3902/Sounds/SoundFactory.cs
+++ cse3902/Sounds/SoundFactory.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
 
 namespace cse3902.Sounds
@@ -16,18 +10,24 @@ namespace cse3902.Sounds
         public SoundEffect enemyDie { get; set; }
         public SoundEffect linkHit { get; set; }
         public SoundEffect linkDie { get; set; }
+        //need to implement
         public SoundEffect swordSlash { get; set; }
+        //need to implement
         public SoundEffect arrowBoomerang { get; set; }
+        //need to implement
         public SoundEffect bombBlow { get; set; }
         public SoundEffect lowHealth { get; set; }
         public SoundEffect getItem { get; set; }
         public SoundEffect getRupee { get; set; }
+        //need to implement
         public SoundEffect text { get; set; }
         public SoundEffect keyAppear { get; set; }
         public SoundEffect doorUnlock { get; set; }
+        //need to implement
         public SoundEffect stairs { get; set; }
         public SoundEffect bossScream { get; set; }
         public SoundEffect bossHurt { get; set; }
+        //need to implement
         public SoundEffect secret { get; set; }
 
         private static SoundFactory instance = new SoundFactory();
@@ -47,8 +47,7 @@ namespace cse3902.Sounds
         public void LoadAllTextures(ContentManager content)
         {
             getHeart = content.Load<SoundEffect>("LOZ_Get_Heart");
-            // enemy hurt was unavailable, check later and update sound file
-            enemyHit = content.Load<SoundEffect>("LOZ_Enemy_Die");
+            enemyHit = content.Load<SoundEffect>("LOZ_Enemy_Hit");
             enemyDie = content.Load<SoundEffect>("LOZ_Enemy_Die");
             linkHit = content.Load<SoundEffect>("LOZ_Link_Hurt");
             linkDie = content.Load<SoundEffect>("LOZ_Link_Die");
@@ -57,16 +56,15 @@ namespace cse3902.Sounds
             bombBlow = content.Load<SoundEffect>("LOZ_Bomb_Blow");
             lowHealth = content.Load<SoundEffect>("LOZ_LowHealth");
             getItem = content.Load<SoundEffect>("LOZ_Get_Item");
-            // change to rupee sound
-            getRupee = content.Load<SoundEffect>("LOZ_Get_Item");
+            getRupee = content.Load<SoundEffect>("LOZ_Get_Rupee");
             text = content.Load<SoundEffect>("LOZ_Text");
             keyAppear = content.Load<SoundEffect>("LOZ_Key_Appear");
             doorUnlock = content.Load<SoundEffect>("LOZ_Door_Unlock");
             stairs = content.Load<SoundEffect>("LOZ_Stairs");
             bossScream = content.Load<SoundEffect>("LOZ_Boss_Scream1");
-            // change to boss hurt
+            // change to boss hurt, currently unavailable
             bossHurt = content.Load<SoundEffect>("LOZ_Boss_Scream1");
-            //secret = content.Load<SoundEffect>("LOZ_Secret");
+            secret = content.Load<SoundEffect>("LOZ_Secret");
         }
 
         public static void PlaySound(SoundEffect sound)
