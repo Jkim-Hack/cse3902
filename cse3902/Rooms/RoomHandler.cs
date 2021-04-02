458dad1b75ceb792478c7fe228477cd7f05e5a82
Pranav <koduri.1@napls.us>
2021-04-02 02:23:15 -0400

    set up code for sprint 5 so room can only be changed by clicking if it's not a wall

--- cse3902/Rooms/RoomHandler.cs
+++ cse3902/Rooms/RoomHandler.cs
@@ -96,6 +96,7 @@ namespace cse3902.Rooms
             if (!roomTransitionManager.IsTransitioning())
             {
                 roomChange += currentRoom;
+                //if (rooms.ContainsKey(roomChange) && !roomChange.Equals(startingRoom) && rooms.GetValueOrDefault(roomChange).Doors[i].State != IDoor.DoorState.Wall) LoadNewRoom(roomChange, rooms.GetValueOrDefault(roomChange).Doors[i]);
                 if (rooms.ContainsKey(roomChange) && !roomChange.Equals(startingRoom)) LoadNewRoom(roomChange, rooms.GetValueOrDefault(roomChange).Doors[i]);
             }
         }
