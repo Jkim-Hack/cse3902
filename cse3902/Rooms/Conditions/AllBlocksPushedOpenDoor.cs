a4a143d64aa74ebb02c3bd016db5a79f63163c79
Pranav <koduri.1@napls.us>
2021-04-02 02:10:48 -0400

    updated room conditions to play door sounds properly

--- cse3902/Rooms/Conditions/AllBlocksPushedOpenDoor.cs
+++ cse3902/Rooms/Conditions/AllBlocksPushedOpenDoor.cs
@@ -1,20 +1,36 @@
 ﻿using cse3902.Interfaces;
 using System.Collections.Generic;
+using cse3902.Sounds;
+using Microsoft.Xna.Framework.Audio;
 
 namespace cse3902.Rooms.Conditions
 {
     public class AllBlocksPushedOpenDoor : ICondition
     {
         private List<int> conditionVariables;
+        private IDoor.DoorState prevState;
 
         public AllBlocksPushedOpenDoor(List<int> condVariables)
         {
             /*
              * condVariables[i] = index of desired door in door list of desired room to be opened
+             * conditionVariables[conditionVariables.Count - 1] = initial state of specified doors
              * 
              * There is no limit to the number of doors this can be done for in each room.
              */
             conditionVariables = condVariables;
+            switch (conditionVariables[conditionVariables.Count - 1])
+            {
+                case 0:
+                    prevState = IDoor.DoorState.Closed;
+                    break;
+                case 1:
+                    prevState = IDoor.DoorState.Open;
+                    break;
+                default: //this should never happen
+                    break;
+            }
+            conditionVariables.RemoveAt(conditionVariables.Count - 1);
         }
 
         public void CheckCondition()
@@ -22,7 +38,10 @@ namespace cse3902.Rooms.Conditions
             IDoor.DoorState doorState = IDoor.DoorState.Open;
             foreach (IBlock block in RoomBlocks.Instance.ListRef)
             {
-                if (!block.IsMoved()) doorState = IDoor.DoorState.Closed;
+                if (!block.IsMoved())
+                {
+                    doorState = IDoor.DoorState.Closed;
+                }
             }
 
             List<IDoor> doors = RoomDoors.Instance.ListRef as List<IDoor>;
@@ -30,6 +49,9 @@ namespace cse3902.Rooms.Conditions
             {
                 doors[doornum].State = doorState;
             }
+
+            if (doorState != prevState) SoundFactory.PlaySound(SoundFactory.Instance.doorUnlock);
+            prevState = doorState;
         }
     }
 }
