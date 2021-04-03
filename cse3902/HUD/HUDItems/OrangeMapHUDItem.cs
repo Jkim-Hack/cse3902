5719e318d8a0cd2f76735995fd886ea915c29139
Dev <dvptl68@gmail.com>
2021-04-03 14:38:20 -0400

    Add room index calculation function

--- cse3902/HUD/HUDItems/OrangeMapHUDItem.cs
+++ cse3902/HUD/HUDItems/OrangeMapHUDItem.cs
@@ -21,7 +21,6 @@ namespace cse3902.HUD
         private Texture2D roomsTexture;
         private Rectangle[] roomFrames;
 
-        /* Each room is stored as its coordinates and doors - int represents a 4-bit integer, modeled as (left, right, top, bottom), with 0 meaning closed and 1 meaning open */
         private Dictionary<Vector3, int> rooms;
 
         private int offsetX;
@@ -55,15 +54,33 @@ namespace cse3902.HUD
             if (game.RoomHandler.currentRoom.Z == 0 && !rooms.ContainsKey(game.RoomHandler.currentRoom))
             {
                 /* change to add correct # for doors */
-                rooms.Add(game.RoomHandler.currentRoom, 15);
+                rooms.Add(game.RoomHandler.currentRoom, GetRoomIndex(game.RoomHandler.rooms[game.RoomHandler.currentRoom].Doors));
             }
 
             return 0;
         }
 
-        public void UpdateRoomDoors(Vector3 coords, int newDoors)
+        private int GetRoomIndex(List<IDoor> roomDoors)
         {
-            rooms[coords] = newDoors;
+            /* List of doors is modeled as (bottom, left, top, right) */
+            /* int represents a 4-bit integer, modeled as (left, right, top, bottom), with 0 meaning closed and 1 meaning open */
+            int doors = 0;
+
+            doors |= IsOpen(roomDoors[1]);
+            doors <<= 1;
+            doors |= IsOpen(roomDoors[3]);
+            doors <<= 1;
+            doors |= IsOpen(roomDoors[2]);
+            doors <<= 1;
+            doors |= IsOpen(roomDoors[0]);
+
+            return doors;
+        }
+
+        private int IsOpen(IDoor door)
+        {
+            if (door.State == IDoor.DoorState.Open || door.State == IDoor.DoorState.Bombed) return 0b1;
+            else return 0b0;
         }
 
         public void Draw()
@@ -80,10 +97,17 @@ namespace cse3902.HUD
 
         private void DrawRooms()
         {
-            foreach (Vector3 coords in rooms.Keys)
+            // foreach (Vector3 coords in rooms.Keys)
+            // {
+            //     Rectangle destination = OrangeMapConstants.CalculatePos(coords, OrangeMapConstants.RoomSize, scaledMapWidth, scaledMapHeight);
+            //     HUDUtilities.DrawTexture(game, roomsTexture, destination, offsetX, offsetY, HUDUtilities.OrangeMapRoomLayer, roomFrames[rooms[coords]]);
+            // }
+            foreach (Vector3 coords in game.RoomHandler.rooms.Keys)
             {
+                if (game.RoomHandler.rooms[coords].Doors.Count != 4) continue;
+                Rectangle frame = roomFrames[GetRoomIndex(game.RoomHandler.rooms[coords].Doors)];
                 Rectangle destination = OrangeMapConstants.CalculatePos(coords, OrangeMapConstants.RoomSize, scaledMapWidth, scaledMapHeight);
-                HUDUtilities.DrawTexture(game, roomsTexture, destination, offsetX, offsetY, HUDUtilities.OrangeMapRoomLayer, roomFrames[rooms[coords]]);
+                HUDUtilities.DrawTexture(game, roomsTexture, destination, offsetX, offsetY, HUDUtilities.OrangeMapRoomLayer, frame);
             }
         }
 
