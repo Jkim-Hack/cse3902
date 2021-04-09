95540f734f3753d9125c8ab10e8fdc5735a9ec85
Dev <dvptl68@gmail.com>
2021-04-09 13:54:07 -0400

    Add particle class

---
+++ cse3902/ParticleSystem/Particle.cs
@@ -0,0 +1,36 @@
+using cse3902.Interfaces;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using System;
+
+namespace cse3902.ParticleSystem
+{
+    public class Particle : IParticle
+    {
+        private Texture2D particle;
+        private Texture2D startingLocation;
+
+        private bool deadParticle;
+
+        public Particle()
+        {
+            deadParticle = false;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw()
+        {
+
+        }
+
+        public bool Dead
+        {
+            get => deadParticle;
+        }
+    }
+}
--- cse3902/ParticleSystem/Particles/StraightParticle.cs
+++
@@ -1,7 +0,0 @@
-using cse3902.Interfaces;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using System.Collections.Generic;
-using System;
-
-
