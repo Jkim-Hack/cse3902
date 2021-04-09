90bf2fcf064b3cef2fb2da7794852a0ad24cf674
Dev <dvptl68@gmail.com>
2021-04-09 13:46:26 -0400

    Add textures to engine

--- cse3902/ParticleSystem/ParticleEngine.cs
+++ cse3902/ParticleSystem/ParticleEngine.cs
@@ -1,5 +1,6 @@
 using cse3902.Interfaces;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Content;
 using System.Collections.Generic;
 using System;
@@ -18,22 +19,32 @@ namespace cse3902.ParticleSystem
 
         private List<IParticleEmmiter> emitters;
 
+        private Texture2D circle;
+        private Texture2D cloud;
+        private Texture2D diamond;
+        private Texture2D star;
+        private Texture2D ring;
+
         private ParticleEngine()
         {
-            // emitters = new List<IParticleEmmiter>();
+            emitters = new List<IParticleEmmiter>();
         }
 
         public void LoadAllTextures(ContentManager content)
         {
-
+            circle = content.Load<Texture2D>("particles/circle");
+            cloud = content.Load<Texture2D>("particles/cloud");
+            diamond = content.Load<Texture2D>("particles/diamond");
+            star = content.Load<Texture2D>("particles/star");
+            ring = content.Load<Texture2D>("particles/ring");
         }
 
-        public void CreateNewEmitter(ParticleEmitter emitter)
+        public void CreateNewEmitter(ParticleEmitter emitter, Vector2 location)
         {
             switch (emitter)
             {
                 case ParticleEmitter.ArrowHit:
-                    emitters.Add(new ArrowEmitter(null, new Vector2()));
+                    emitters.Add(new ArrowEmitter(cloud, location));
                     break;
 
                 default:
