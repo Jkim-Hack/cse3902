52f1e0a5fabcb834062432dc911917f1a5caf469
Dev <dvptl68@gmail.com>
2021-04-09 13:05:43 -0400

    Begin engine structure

--- cse3902/Interfaces/IParticleEmitter.cs
+++ cse3902/Interfaces/IParticleEmitter.cs
@@ -4,6 +4,8 @@ namespace cse3902.Interfaces
 {
     public interface IParticleEmmiter
     {
+        public bool AnimationDone { get; }
+
         public void Update(GameTime gameTime);
         public void Draw();
     }
--- cse3902/ParticleEngine/ParticleEngine.cs
+++ cse3902/ParticleEngine/ParticleEngine.cs
@@ -1,18 +1,50 @@
 using cse3902.Interfaces;
+using System.Collections.Generic;
 using System;
 
 namespace cse3902.ParticleEngine
 {
     public class ParticleEngine
     {
+        public enum ParticleEmitter
+        {
+            ArrowHit
+        }
+
         private static ParticleEngine instance = new ParticleEngine();
         public static ParticleEngine Instance { get => instance; }
 
-        private ParticleEngine() {}
+        List<IParticleEmmiter> emitters;
+
+        private ParticleEngine()
+        {
+            emitters = new List<IParticleEmmiter>();
+        }
 
         public void LoadAllTextures()
         {
 
+        }
+
+        public void CreateNewEmitter(ParticleEmitter emitter)
+        {
+            switch (emitter)
+            {
+                case ParticleEmitter.ArrowHit:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw()
+        {
+            
         }
     }
 }
