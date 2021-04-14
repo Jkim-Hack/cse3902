51ff3e460844938028bf39f5efdee2b3f802884d
John Kim <johnkim1108@gmail.com>
2021-04-13 23:51:11 -0400

    Add spawner parser and ISpawner

---
+++ cse3902/Interfaces/ISpawner.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace cse3902.Interfaces
+{
+    public interface ISpawner
+    {
+        public ref Rectangle Bounds { get; }
+        public Vector2 Center { get; }
+        public void Update();
+        public void Draw();
+        public void Reset();
+    }
+}
