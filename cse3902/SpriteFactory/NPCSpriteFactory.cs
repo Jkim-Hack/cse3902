fa047d8e8d7fe445f6847b9230fd59b449a67831
Pranav <koduri.1@napls.us>
2021-04-15 03:33:43 -0400

    added settings sprites

--- cse3902/SpriteFactory/NPCSpriteFactory.cs
+++ cse3902/SpriteFactory/NPCSpriteFactory.cs
@@ -31,9 +31,15 @@ namespace cse3902.SpriteFactory
             npcTextures.Add("merchant", content.Load<Texture2D>("merchant"));
             npcTextures.Add("grabbedlink", content.Load<Texture2D>("GrabbedLink"));
 
-            //npcTextures.Add("easy", content.Load<Texture2D>("easy"));
-            //npcTextures.Add("normal", content.Load<Texture2D>("normal"));
-            //npcTextures.Add("hard", content.Load<Texture2D>("hard"));
+            npcTextures.Add("easy", content.Load<Texture2D>("easy"));
+            npcTextures.Add("normal", content.Load<Texture2D>("normal"));
+            npcTextures.Add("hard", content.Load<Texture2D>("hard"));
+            npcTextures.Add("utilities", content.Load<Texture2D>("utilities"));
+            npcTextures.Add("vision", content.Load<Texture2D>("vision"));
+            npcTextures.Add("item", content.Load<Texture2D>("itemdroprate"));
+            npcTextures.Add("enemy", content.Load<Texture2D>("enemystrength"));
+            npcTextures.Add("sword", content.Load<Texture2D>("minprojhealth"));
+            npcTextures.Add("health", content.Load<Texture2D>("health"));
 
             flame = content.Load<Texture2D>("fire");
         }
@@ -71,6 +77,36 @@ namespace cse3902.SpriteFactory
         public ISprite CreateHardSprite(SpriteBatch spriteBatch, Vector2 startingPos)
         {
             return new SettingsSprite(spriteBatch, npcTextures["hard"], startingPos);
+        }
+
+        public ISprite CreateUtilitiesSprite(SpriteBatch spriteBatch, Vector2 startingPos)
+        {
+            return new SettingsSprite(spriteBatch, npcTextures["utilities"], startingPos);
+        }
+
+        public ISprite CreateVisionSprite(SpriteBatch spriteBatch, Vector2 startingPos)
+        {
+            return new SettingsSprite(spriteBatch, npcTextures["vision"], startingPos);
+        }
+
+        public ISprite CreateItemSprite(SpriteBatch spriteBatch, Vector2 startingPos)
+        {
+            return new SettingsSprite(spriteBatch, npcTextures["item"], startingPos);
+        }
+
+        public ISprite CreateEnemySprite(SpriteBatch spriteBatch, Vector2 startingPos)
+        {
+            return new SettingsSprite(spriteBatch, npcTextures["enemy"], startingPos);
+        }
+
+        public ISprite CreateSwordSprite(SpriteBatch spriteBatch, Vector2 startingPos)
+        {
+            return new SettingsSprite(spriteBatch, npcTextures["sword"], startingPos);
+        }
+
+        public ISprite CreateHealthSprite(SpriteBatch spriteBatch, Vector2 startingPos)
+        {
+            return new SettingsSprite(spriteBatch, npcTextures["health"], startingPos);
         }
 
         public ISprite CreateFlameSprite(SpriteBatch spriteBatch, Vector2 startingPos)
