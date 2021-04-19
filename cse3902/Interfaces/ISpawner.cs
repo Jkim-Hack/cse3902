d8e513e05b216da2c3a6be65197f1a2e5fc47c98
John Kim <johnkim1108@gmail.com>
2021-04-19 14:40:32 -0400

    Minor changes

--- cse3902/Interfaces/ISpawner.cs
+++ cse3902/Interfaces/ISpawner.cs
@@ -6,6 +6,7 @@ namespace cse3902.Interfaces
     public interface ISpawner
     {
         public ref Rectangle Bounds { get; }
+        public int Count { get; }
         public Vector2 Center { get; }
         public void Update();
         public void Draw();
