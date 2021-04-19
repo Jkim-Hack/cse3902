01ab6b89adddaca475c693d57c0e42da71121116
Andrew Fecher <andrew.fecher@gmail.com>
2021-04-18 20:17:08 -0400

    Added sword changes

--- cse3902/Constants/DamageConstants.cs
+++ cse3902/Constants/DamageConstants.cs
@@ -5,6 +5,11 @@
 	    public const float DamageDisableDelay = 1.0f;
 	    public const float DamageMaskDelay = 0.05f;
 
+        public const int WOODENSWORDDAMAGE = 6;
+        public const int WHITESWORDDAMAGE = 12;
+
+
+
         public static int SwordDamage = 1;
     }
 }
