cc764a71b09f10accbe711c89066d23ca381c53a
smerapala <smerapala@gmail.com>
2021-04-22 16:24:30 -0400

    Fix magic numbers in MapCompasshud

---
+++ cse3902/Constants/MapCompassConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace cse3902.Constants
+{
+    public class MapCompassConstants
+    {
+        public const float CompassScalar = 1.3f;
+
+        public const int XOffset = 35;
+
+        public const int CompassLabelHeightOffset = 5;
+    }
+}
